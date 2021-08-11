e7c33820446319c9f6c82c78679bdf0eda357c91
Luis J. Montes <luisjavm3@gmail.com>
2021-08-11 14:53:05 -0500

    Create links on Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header />
 
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
 
       <Footer />
-    </Fragment>
+    </Router>
   );
 }
 
