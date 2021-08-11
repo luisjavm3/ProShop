51e3bed00f86a0c8ef2989601eae0034d47c7da4
Luis J. Montes <luisjavm3@gmail.com>
2021-08-11 14:26:10 -0500

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
