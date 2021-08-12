837c576332511f5fbf6d0e32b4cc075c1f57c3d0
Luis J. Montes <luisjavm3@gmail.com>
2021-08-12 10:17:04 -0500

    Move connecDB method to a independ file

---
+++ backend/config/connectDB.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  const MONGO_URI = process.env.MONGO_URI;
+
+  try {
+    const conn = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
