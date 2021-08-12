d739eb36bbe23dc688ebaf1192d9c1320e4d46a1
Luis J. Montes <luisjavm3@gmail.com>
2021-08-12 09:29:11 -0500

    Add .env file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const products = require('./data/products');
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -20,6 +22,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, () => {
-  console.log('Server Listening on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on port ${PORT}`);
 });
