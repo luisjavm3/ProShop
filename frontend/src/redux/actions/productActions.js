e312976ef5fa52e45e2cc8add551f633b6d36d03
Luis J. Montes <luisjavm3@gmail.com>
2021-08-13 09:15:08 -0500

    Wire state with HomeScreen view

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -14,6 +14,12 @@ export const listProducts = () => async (dispatch) => {
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
-    dispatch({ type: PRODUCT_LIST_FAIL, payload: error });
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
