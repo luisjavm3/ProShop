70eb81f8865014e3ecd6449e595718e5fb9b629d
Luis J. Montes <luisjavm3@gmail.com>
2021-08-13 12:09:57 -0500

    Save and load the cart from localStorage

---
+++ frontend/src/redux/reducers/cartReducers.js
@@ -0,0 +1,29 @@
+import { CART_ADD_ITEM } from '../actionTypes';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ADD_ITEM:
+      const item = payload;
+
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+
+    default:
+      return state;
+  }
+};
